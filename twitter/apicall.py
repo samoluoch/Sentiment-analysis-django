94361fb4cce1460db017a79859d8fbad19305a70
Ian Odhiambo <you@example.com>
2018-11-01 14:38:43 +0300

    pushing

--- twitter/apicall.py
+++ twitter/apicall.py
@@ -3,14 +3,6 @@ import tweepy
 from textblob import TextBlob
 
 
-# class Oauth_Secrets:
-#     def __init__(self):
-#         self.consumer_key = "WRxcgZHq8HOA9AiACeoz7pc61"
-#         self.consumer_secret = "IJnOARqL3baljF5VfMPB4Gy1GmxVLlSv6L4BgJoh3bVDslSQYL"
-#         self.access_token = "32554005-yIgL0lbl0aWXyJ0E8q61zDF8BpOtVzWwRoZyCDm1n"
-#         self.access_token_secret = "PzknR8jcAmNgG35G0D99BH9qEJfF7n477AxK1kgFDnVWl"
-
-
 def getdata(input_hashtag):
 
     # input_hashtag = 'obama'
